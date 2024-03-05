df64fa2f8fd7c5618327a784c69911ea86c2f1f7
JoaoVellenich <jpvellenich@gmail.com>
2024-03-05 18:38:12 -0300

    feat: create car controller

--- cars/src/main/java/com/joaovellenich/cars/modules/cars/controllers/CarsController.java
+++ cars/src/main/java/com/joaovellenich/cars/modules/cars/controllers/CarsController.java
@@ -1,6 +1,11 @@
 package com.joaovellenich.cars.modules.cars.controllers;
 
+import com.joaovellenich.cars.infra.security.TokenService;
+import com.joaovellenich.cars.modules.user.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("cars")
 public class CarsController {
-    @GetMapping("/getCars")
+    @Autowired
+    private TokenService tokenService;
+    @GetMapping("/createCar")
     public ResponseEntity getAllCars(){
-        return ResponseEntity.ok().body("Teste");
+        try{
+            var user = this.tokenService.getUsr();
+            System.out.println(user);
+            return ResponseEntity.ok().body(user);
+        }catch (Exception error){
+            return ResponseEntity.badRequest().body(error.getMessage());
+        }
     }
 }
