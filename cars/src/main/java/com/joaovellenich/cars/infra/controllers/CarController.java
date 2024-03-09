db31bc12c331bc780340b1eac72338bfd838f7fa
JoaoVellenich <jpvellenich@gmail.com>
2024-03-09 10:53:29 -0300

    feat: Adding get cars route

--- cars/src/main/java/com/joaovellenich/cars/infra/controllers/CarController.java
+++ cars/src/main/java/com/joaovellenich/cars/infra/controllers/CarController.java
@@ -2,24 +2,35 @@ package com.joaovellenich.cars.infra.controllers;
 
 
 import com.joaovellenich.cars.application.usecases.CreateCarUseCase;
+import com.joaovellenich.cars.application.usecases.GetCarByOwnerIdUseCase;
 import com.joaovellenich.cars.domain.Car;
 import com.joaovellenich.cars.dto.createcarDTO.CreateCarDTOMapper;
 import com.joaovellenich.cars.dto.createcarDTO.CreateCarRequest;
 import com.joaovellenich.cars.dto.createcarDTO.CreateCarResponse;
+import com.joaovellenich.cars.dto.getcarsDTO.GetCarsDTOMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
     private final CreateCarUseCase createCarUseCase;
     private final CreateCarDTOMapper createCarDTOMapper;
-    public CarController(CreateCarUseCase createCarUseCase, CreateCarDTOMapper createCarDTOMapper){
+    private final GetCarByOwnerIdUseCase getCarByOwnerIdUseCase;
+    private final GetCarsDTOMapper getCarsDTOMapper;
+    public CarController(
+            CreateCarUseCase createCarUseCase,
+            CreateCarDTOMapper createCarDTOMapper,
+            GetCarByOwnerIdUseCase getCarByOwnerIdUseCase,
+            GetCarsDTOMapper getCarsDTOMapper
+    ){
         this.createCarUseCase = createCarUseCase;
         this.createCarDTOMapper = createCarDTOMapper;
+        this.getCarByOwnerIdUseCase = getCarByOwnerIdUseCase;
+        this.getCarsDTOMapper = getCarsDTOMapper;
     }
     @PostMapping("/")
     public ResponseEntity<CreateCarResponse> createCar(@RequestBody CreateCarRequest request){
@@ -27,6 +38,18 @@ public class CarController {
             Car newCar = this.createCarDTOMapper.toCar(request);
             Car savedCar = this.createCarUseCase.createCar(newCar);
             return ResponseEntity.ok().body(this.createCarDTOMapper.toCreateCarResponse(savedCar));
+        }catch (Exception error){
+            System.out.println(error);
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Car>> getCarsByOwnerId(){
+        try{
+            UUID ownerId = this.getCarsDTOMapper.getOwnerUUID();
+            List<Car> cars = this.getCarByOwnerIdUseCase.getCarByOwnerId(ownerId);
+            return ResponseEntity.ok().body(cars);
         }catch (Exception error){
             System.out.println(error);
             return ResponseEntity.badRequest().build();
