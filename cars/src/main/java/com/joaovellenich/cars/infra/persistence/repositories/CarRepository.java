7dc9ef7a943139119b442fe901bdd28405cdf233
JoaoVellenich <jpvellenich@gmail.com>
2024-03-09 10:54:03 -0300

    feat: Adding get cars by owner id to repository

--- cars/src/main/java/com/joaovellenich/cars/infra/persistence/repositories/CarRepository.java
+++ cars/src/main/java/com/joaovellenich/cars/infra/persistence/repositories/CarRepository.java
@@ -2,8 +2,12 @@ package com.joaovellenich.cars.infra.persistence.repositories;
 
 import com.joaovellenich.cars.infra.persistence.entity.CarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface CarRepository extends JpaRepository<CarEntity, UUID> {
+    public List<CarEntity> findByOwnerId(UUID ownerId);
 }
