6fa0ad5e2e104a819cda97de855b923ad8fb77e4
JoaoVellenich <jpvellenich@gmail.com>
2024-03-09 11:05:33 -0300

    feat: Adding get cars by owner id to repository

--- cars/src/main/java/com/joaovellenich/cars/infra/gateways/CarRepositoryGateway.java
+++ cars/src/main/java/com/joaovellenich/cars/infra/gateways/CarRepositoryGateway.java
@@ -6,6 +6,10 @@ import com.joaovellenich.cars.infra.persistence.entity.CarEntity;
 import com.joaovellenich.cars.infra.persistence.mapper.CarEntityMapper;
 import com.joaovellenich.cars.infra.persistence.repositories.CarRepository;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 public class CarRepositoryGateway implements CarGateway {
     private final CarRepository carRepository;
     private final CarEntityMapper carEntityMapper;
@@ -19,5 +23,12 @@ public class CarRepositoryGateway implements CarGateway {
         CarEntity newCarEntity = this.carEntityMapper.toEntity(car);
         CarEntity savedCar = this.carRepository.save(newCarEntity);
         return this.carEntityMapper.toDomain(savedCar);
+    }
+
+    @Override
+    public List<Car> getCarByOwnerId(UUID ownerId) {
+        List<CarEntity> carsEntity = this.carRepository.findByOwnerId(ownerId);
+        List<Car> carsDomain = carsEntity.stream().map(carEntityMapper::toDomain).toList();
+        return carsDomain;
     }
 }
