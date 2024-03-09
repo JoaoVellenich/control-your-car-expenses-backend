0e5e6910aead652549891682441ee9138654dd3c
JoaoVellenich <jpvellenich@gmail.com>
2024-03-09 10:53:50 -0300

    feat: Adding get cars function to gateway

--- cars/src/main/java/com/joaovellenich/cars/application/gateways/CarGateway.java
+++ cars/src/main/java/com/joaovellenich/cars/application/gateways/CarGateway.java
@@ -2,6 +2,10 @@ package com.joaovellenich.cars.application.gateways;
 
 import com.joaovellenich.cars.domain.Car;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface CarGateway {
     public Car createCar(Car car);
+    public List<Car> getCarByOwnerId(UUID ownerId);
 }
