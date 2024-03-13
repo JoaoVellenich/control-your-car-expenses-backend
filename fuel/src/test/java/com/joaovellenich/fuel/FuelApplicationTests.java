cfde3fb92e665f233e32332047ed0e9a4cc9ddcb
JoaoVellenich <jpvellenich@gmail.com>
2024-03-13 18:18:16 -0300

    feat: start fuel API

---
+++ fuel/src/test/java/com/joaovellenich/fuel/FuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovellenich.fuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
