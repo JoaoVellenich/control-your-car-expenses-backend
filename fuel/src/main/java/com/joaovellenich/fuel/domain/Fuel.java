37cdf15e22c51f4812220c433ada25a37767f696
JoaoVellenich <jpvellenich@gmail.com>
2024-03-18 20:22:11 -0300

    fix: remove getter and setters from furl domain

--- fuel/src/main/java/com/joaovellenich/fuel/domain/Fuel.java
+++ fuel/src/main/java/com/joaovellenich/fuel/domain/Fuel.java
@@ -1,15 +1,11 @@
 package com.joaovellenich.fuel.domain;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Builder
-@Setter
-@Getter
 public record Fuel (
        UUID id,
        LocalDateTime date,
