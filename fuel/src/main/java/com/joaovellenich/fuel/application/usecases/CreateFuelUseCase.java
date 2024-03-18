b8c367f7b4bad23114ad01b8639881b466622aa9
JoaoVellenich <jpvellenich@gmail.com>
2024-03-18 18:58:34 -0300

    feat: creating CreateFuelUseCase

---
+++ fuel/src/main/java/com/joaovellenich/fuel/application/usecases/CreateFuelUseCase.java
@@ -0,0 +1,21 @@
+package com.joaovellenich.fuel.application.usecases;
+
+import com.joaovellenich.fuel.application.gateways.FuelGateway;
+import com.joaovellenich.fuel.domain.Fuel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateFuelUseCase {
+    private static Logger logger = LoggerFactory.getLogger(CreateFuelUseCase.class);
+    private final FuelGateway fuelGateway;
+    public CreateFuelUseCase(FuelGateway fuelGateway){
+        this.fuelGateway = fuelGateway;
+    }
+
+    public Fuel createFuel(Fuel fuel){
+        logger.info("Start createFuel UseCase - Fuel - " + fuel);
+       Fuel savedFuel = this.fuelGateway.createFuel(fuel);
+       logger.info("Finished createFuel UseCase - SavedFuel - " + savedFuel);
+       return savedFuel;
+    }
+}
