ab0c96cf25fdde60776471387554cb6827708a33
JoaoVellenich <jpvellenich@gmail.com>
2024-03-18 20:21:37 -0300

    feat: adding create fuel use case and DTO mapper to config

--- fuel/src/main/java/com/joaovellenich/fuel/main/FuelConfig.java
+++ fuel/src/main/java/com/joaovellenich/fuel/main/FuelConfig.java
@@ -1,6 +1,8 @@
 package com.joaovellenich.fuel.main;
 
 import com.joaovellenich.fuel.application.gateways.FuelGateway;
+import com.joaovellenich.fuel.application.usecases.CreateFuelUseCase;
+import com.joaovellenich.fuel.dto.createfuelDTO.CreateFuelDTOMapper;
 import com.joaovellenich.fuel.infra.gateways.FuelRepositoryGateway;
 import com.joaovellenich.fuel.infra.persistence.mapper.FuelEntityMapper;
 import com.joaovellenich.fuel.infra.persistence.repositories.FuelRepository;
@@ -10,11 +12,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FuelConfig {
     @Bean
+    public CreateFuelUseCase createFuelUseCase(FuelGateway fuelGateway){
+        return new CreateFuelUseCase(fuelGateway);
+    }
+    @Bean
     public FuelGateway fuelGateway(FuelRepository fuelRepository, FuelEntityMapper fuelEntityMapper){
         return new FuelRepositoryGateway(fuelRepository, fuelEntityMapper);
     }
     @Bean
     public FuelEntityMapper fuelEntityMapper(){
         return new FuelEntityMapper();
+    }
+    @Bean
+    public CreateFuelDTOMapper createFuelDTOMapper(){
+        return new CreateFuelDTOMapper();
     }
 }
