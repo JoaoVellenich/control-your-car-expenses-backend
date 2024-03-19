284db0097c00361e6d01371d20ff8503c16f8102
JoaoVellenich <jpvellenich@gmail.com>
2024-03-19 19:04:37 -0300

    feat: finished create fuel use case

--- fuel/src/main/java/com/joaovellenich/fuel/application/usecases/CreateFuelUseCase.java
+++ fuel/src/main/java/com/joaovellenich/fuel/application/usecases/CreateFuelUseCase.java
@@ -1,5 +1,6 @@
 package com.joaovellenich.fuel.application.usecases;
 
+import com.joaovellenich.fuel.application.gateways.CarGateway;
 import com.joaovellenich.fuel.application.gateways.FuelGateway;
 import com.joaovellenich.fuel.domain.Fuel;
 import org.slf4j.Logger;
@@ -8,14 +9,25 @@ import org.slf4j.LoggerFactory;
 public class CreateFuelUseCase {
     private final static Logger logger = LoggerFactory.getLogger(CreateFuelUseCase.class);
     private final FuelGateway fuelGateway;
-    public CreateFuelUseCase(FuelGateway fuelGateway){
+    private final CarGateway carGateway;
+    public CreateFuelUseCase(
+            FuelGateway fuelGateway,
+            CarGateway carGateway
+    ){
         this.fuelGateway = fuelGateway;
+        this.carGateway = carGateway;
     }
 
-    public Fuel createFuel(Fuel fuel){
-        logger.info("Start createFuel UseCase - Fuel - " + fuel);
-       Fuel savedFuel = this.fuelGateway.createFuel(fuel);
-       logger.info("Finished createFuel UseCase - SavedFuel - " + savedFuel);
-       return savedFuel;
+    public Fuel createFuel(Fuel fuel, String token) throws Exception{
+        try{
+            logger.info("Start createFuel UseCase - Fuel - " + fuel);
+            Fuel savedFuel = this.fuelGateway.createFuel(fuel);
+            this.carGateway.updateCarKm(fuel.carId(), fuel.km(), token);
+            logger.info("Finished createFuel UseCase - SavedFuel - " + savedFuel);
+            return savedFuel;
+        }catch (Exception error){
+            logger.error("Error createFuel UseCase - Error - " + error);
+            throw new Exception("Error to create fuel");
+        }
     }
 }
