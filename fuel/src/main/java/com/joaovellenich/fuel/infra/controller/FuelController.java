e3b7adb73f8225f4c0c68a2a03509712a863f8a7
JoaoVellenich <jpvellenich@gmail.com>
2024-03-25 19:03:40 -0300

    feat: adding route to update fuel

--- fuel/src/main/java/com/joaovellenich/fuel/infra/controller/FuelController.java
+++ fuel/src/main/java/com/joaovellenich/fuel/infra/controller/FuelController.java
@@ -1,10 +1,13 @@
 package com.joaovellenich.fuel.infra.controller;
 
 import com.joaovellenich.fuel.application.usecases.CreateFuelUseCase;
+import com.joaovellenich.fuel.application.usecases.EditFuelUseCase;
 import com.joaovellenich.fuel.application.usecases.GetAllFuelUseCase;
 import com.joaovellenich.fuel.domain.Fuel;
 import com.joaovellenich.fuel.dto.createfuelDTO.CreateFuelDTOMapper;
 import com.joaovellenich.fuel.dto.createfuelDTO.CreateFuelRequest;
+import com.joaovellenich.fuel.dto.updatefuelDTO.UpdateFuelDTOMapper;
+import com.joaovellenich.fuel.dto.updatefuelDTO.UpdateFuelRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +26,21 @@ public class FuelController {
     private final CreateFuelDTOMapper ceCreateFuelDTOMapper;
 
     private final GetAllFuelUseCase getAllFuelUseCase;
+
+    private final EditFuelUseCase editFuelUseCase;
+    private final UpdateFuelDTOMapper updateFuelDTOMapper;
     public FuelController(
             CreateFuelUseCase createFuelUseCase,
             CreateFuelDTOMapper ceCreateFuelDTOMapper,
-            GetAllFuelUseCase getAllFuelUseCase
+            GetAllFuelUseCase getAllFuelUseCase,
+            EditFuelUseCase editFuelUseCase,
+            UpdateFuelDTOMapper updateFuelDTOMapper
     ){
         this.createFuelUseCase = createFuelUseCase;
         this.ceCreateFuelDTOMapper = ceCreateFuelDTOMapper;
         this.getAllFuelUseCase = getAllFuelUseCase;
+        this.editFuelUseCase = editFuelUseCase;
+        this.updateFuelDTOMapper = updateFuelDTOMapper;
     }
 
     @PostMapping("/")
@@ -48,12 +58,26 @@ public class FuelController {
         }
     }
 
-    @GetMapping("/{carId}")
-    public ResponseEntity getAllFuel(@PathVariable UUID carId) {
-        logger.info("Start getAllFuel route - Request - " + carId);
-        List<Fuel> fuel = this.getAllFuelUseCase.getAllFuel(carId);
+    @GetMapping("/{fuelId}")
+    public ResponseEntity getAllFuel(@PathVariable UUID fuelId) {
+        logger.info("Start getAllFuel route - Request - " + fuelId);
+        List<Fuel> fuel = this.getAllFuelUseCase.getAllFuel(fuelId);
         logger.info("Finished getAllUsers - Response - " + fuel);
         return ResponseEntity.ok().body(fuel);
+    }
+
+    @PatchMapping("/{fuelId}")
+    public ResponseEntity updateFuel(@PathVariable UUID fuelId, @RequestBody UpdateFuelRequest request){
+        try{
+            logger.info("Start updateFuel route - Request - " + request);
+            Fuel fuelToUpdate = this.updateFuelDTOMapper.toDomain(request, fuelId);
+            Fuel updatedFuel = this.editFuelUseCase.editFuel(fuelId, fuelToUpdate);
+            logger.info("Finished updateFuel route - Response - " + updatedFuel );
+            return ResponseEntity.ok().body(updatedFuel);
+        }catch (Exception error){
+            logger.error("Error updateFuel - Error - " + error.getMessage());
+            return ResponseEntity.badRequest().body(error.getMessage());
+        }
     }
 
 }
